00af09723451ad82a6c05ac5ccbe7fd9cd779f9c
Mohit Kumar <mkmkbhojawas@gmail.com>
2025-10-03 20:57:23 +0530

    feat: Implementation countdown timer component

--- apps/web-react/src/pages/Home.jsx
+++ apps/web-react/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Gallery from '../components/Home/Gallery/Gallery'
+import CountdownTimer from '../components/Home/Timer/CountdownTimer.jsx'; 
+
 function Home() {
   return (
     <div>
+      <CountdownTimer />
       <Gallery/>
     </div>
   )
